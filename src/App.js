07ce315e5f2a15477f207eb2e03d1d073218499e
Scott <starkey9000@gmail.com>
2018-01-17 23:08:18 -0500

    add bootstrap and begin basic layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RunnerListContainer from './components/RunnerListContainer/RunnerListContainer';
+import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Row>
+                    <Col md={4}>
+                        <RunnerListContainer />
+                    </Col>
+                    <Col md={8}>
+                        this is the user info side
+                    </Col>
+                </Row>
+
+            </div>
+        );
+    }
 }
 
 export default App;
